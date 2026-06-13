ad8336253899170c21f41d2c96808465a4d3bb71
tamjid-research <tamjid.hussein88@gmail.com>
2026-06-13 19:36:46 +0600

    Seventh Commit: Add basic error handling to the calculator

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 from datetime import date
-from utils import add, subtract, multiply
+from utils import add, subtract, multiply, divide
 
 print("Name: Tamjid Hussein")
 print("Today's Date:", date.today())
+
 print("Addition:", add(10, 5))
 print("Subtraction:", subtract(10, 5))
 print("Multiplication:", multiply(10, 5))
+
+print("Division:", divide(10, 2))
+print("Division by Zero:", divide(10, 0))
