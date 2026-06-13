34fd60f8b6a6fe0c87223b37fb055a325ebe06fe
tamjid-research <tamjid.hussein88@gmail.com>
2026-06-13 16:59:01 +0600

    Third Commit: Add basic calculator functions

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
 from datetime import date
+from utils import add, subtract
 
 print("Name: Tamjid Hussein")
 print("Today's Date:", date.today())
+print("Addition:", add(10, 5))
+print("Subtraction:", subtract(10, 5))
+
